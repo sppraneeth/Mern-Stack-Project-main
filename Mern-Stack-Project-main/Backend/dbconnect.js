f023b665d921357bc1909d9f29186e4a096efd77
Praneeth <praneethsurya031 @gamil.com>
2024-07-16 17:09:01 +0530

    first commit

---
+++ Mern-Stack-Project-main/Backend/dbconnect.js
@@ -0,0 +1,24 @@
+// STEP-1 : IMPORT MONGOOSE PACKAGE
+const mongoose = require('mongoose');
+
+// Database Connection URL
+//Mongoose is an Object Document Mapper (ODM)
+// const url = 'mongodb://0.0.0.0/ProjectTest1';
+const url = 'mongodb+srv://kshashidhar144:shashidhar@backenddb.frzjzzz.mongodb.net/doctordb?retryWrites=true&w=majority&appName=BackendDb'
+
+// STEP-2 : ESTABLISH CONNECTION WITH MONGODB DATABASE THROUGH MONGOOSE
+// err is callback function Parameter. ARROW OPERATOR.
+// JSON.stringify convert Object to String. 2 means Indentation of Two space Character 
+mongoose.connect(url)
+      .then( () => 
+             {
+               console.log('NODEJS TO MongoDB Connection ESTABLISH.....');
+             })
+      .catch( err => 
+              {
+               console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
+               process.exit();
+              }); 
+    
+// STEP-3 : EXPORT MODULE mongoose because we need it in other JS file
+module.exports = mongoose;
---
+++ Mern-Stack-Project-main/README.md
@@ -0,0 +1,40 @@
+# Doctor_Appointment_Services
+
+In frontend you have to install modules
+
+    "axios": "^0.27.2"
+    
+    "react": "^18.1.0"
+    
+    "react-dom": "^18.1.0"
+    
+    "react-icons": "^4.4.0"
+    
+    "react-router-dom": "^6.3.0"
+    
+    "react-scripts": "5.0.1"
+    
+    "web-vitals": "^2.1.4"
+    
+In backend you need to install this modules 
+
+    "bcrypt": "^5.0.1"
+    
+    "cors": "^2.8.5"
+    
+    "express": "^4.18.1"
+    
+    "mongoose": "^6.4.0"
+    
+    "nodemailer": "^6.7.5"
+    
+    "randomatic": "^3.1.1"
+    
+    
+WE HAVE DONE THIS PROJECT TO REDUSE THE WASTAGE OF TIME TO BOOK APPOINTMENT IN THE STANDING IN A LINE,IN THIS USER CAN BOOK THE APPOINTMENT IN ANY PLACE
+
+Website Live Link : https://doctorappointmentservices.netlify.app/
+          (or) 
+https://doc-appointment-services.onrender.com/
+                    
+    
